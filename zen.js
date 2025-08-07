cc927680186b2b0e72e28baa65aa9c92dca399ad
aryank1686 <kumararyan10101010@gmail.com>
2025-08-07 21:37:23 +0530

    basic version finished

--- zen.js
+++ zen.js
@@ -1,27 +1,40 @@
-const buttons = document.querySelectorAll(".btn");
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.classList.contains("clear")) {
-      type.value = type.value.slice(0, -1);
-    } else if (button.textContent === "AC") {
-      type.value = "";
-    } else if (
-      button.classList.contains("add") ||
-      button.classList.contains("minus") ||
-      button.classList.contains("divide") ||
-      button.classList.contains("multiply")
-    ) {
-      type.value += ` ${button.textContent} `;
-    } else if (button.textContent === "=") {
-    } else {
-      type.value += button.textContent;
-    }
+// INCOMPLETE RIGHT NOW - FIX BROKEN PARTS AFTER GAINING EXPERIENCE
 
-    type.focus();
-  });
+const image = document.querySelector(".image");
+const body = document.querySelector("body");
+const container = document.querySelector(".calculator");
+const input = document.querySelector("input");
+
+input.focus();
+let lightMode = false;
+image.addEventListener("click", () => {
+  if (lightMode === true) {
+    body.style.backgroundColor = "black";
+    container.style.backgroundColor = "black";
+    input.style.backgroundColor = "black";
+    input.style.color = "white";
+  } else {
+    body.style.backgroundColor = "white";
+    container.style.backgroundColor = "white";
+    input.style.backgroundColor = "white";
+    input.style.color = "black";
+  }
+  lightMode = !lightMode;
+  input.focus();
 });
 
-type.addEventListener("input", function () {
-  const notAllowedPattern = /[^0-9+\-*/().]/g;
-  this.value = this.value.replace(notAllowedPattern, "");
+buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.textContent === "=") {
+      input.value = eval(input.value);
+    } else if (e.target.textContent === "AC") {
+      input.value = "";
+    } else if (button.classList.contains("clear")) {
+      input.value = input.value.slice(0, -1);
+    } else {
+      input.value += e.target.textContent;
+    }
+    input.focus();
+  });
 });
